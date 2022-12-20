e7387b83a437bfcec14cdfb180b12de7767df01f
Tobi <dinhnguyen2002asd@gmail.com>
2022-12-20 15:59:53 +0700

    new server

---
+++ src/main/java/com/dinhnguyen/controller/admin/api/ListMemberGroupAPI.java
@@ -0,0 +1,34 @@
+package com.dinhnguyen.controller.admin.api;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dinhnguyen.model.CTNhomChat;
+import com.dinhnguyen.model.NhomChat;
+import com.dinhnguyen.model.UserModel;
+import com.dinhnguyen.service.IUserService;
+import com.dinhnguyen.util.httpUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet(urlPatterns = {"/api-admin-listmembergroupAPI"})
+public class ListMemberGroupAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Inject
+	private IUserService userService;
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		req.setCharacterEncoding("UTF-8");
+		res.setContentType("application/json");
+		NhomChat ct = httpUtil.of(req.getReader()).toUsers(NhomChat.class);
+		List<UserModel> list = userService.findAllbyid_nhomchat(ct.getId_nhomchat());
+		mapper.writeValue(res.getOutputStream(), list);
+	}
+}
